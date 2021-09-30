7fb7e4712747d3837ca3274c51223fc20bc91754
TYH71 <yuhoetan@gmail.com>
2021-09-30 19:39:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,6 @@ if buffer_file is not None:
     # Show Image
     col1.image(img_data, caption='Uploaded Image', use_column_width=True)
 
-    
     # Generating Image
     km = predict(img_data)
 
