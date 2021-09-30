f3df7cb67af3fad2994d98da4a358bc1ee0ba5da
TYH71 <yuhoetan@gmail.com>
2021-09-30 22:19:57 +0800

    Update plot_utils.py

--- assets/plot_utils.py
+++ assets/plot_utils.py
@@ -19,9 +19,9 @@ class plot_utils:
         R, G, B = self.img_data[index].T
 
         fig, ax = plt.subplots(1, 2, figsize=(15, 9), tight_layout=True)
-        ax[0].scatter(R, G, colors=colors, marker='.')
+        ax[0].scatter(R, G, color=colors, marker='.')
         ax[0].set(xlabel='Red', ylabel='Green', xlim=(0, 1), ylim=(0, 1))
-        ax[1].scatter(R, B, colors=colors, marker='.')
+        ax[1].scatter(R, B, color=colors, marker='.')
         ax[1].set(xlabel='Red', ylabel='Blue', xlim=(0, 1), ylim=(0, 1))
         fig.suptitle(self.title, size=28)
 
